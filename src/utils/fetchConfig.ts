a58fcfa7fcdd9beba89f52dcaa6f83aa5d17ea55
dmytrolesyk <dmitriylesik@gmail.com>
2025-01-07 10:45:51 +0200

    add env var

--- src/utils/fetchConfig.ts
+++ src/utils/fetchConfig.ts
@@ -1,6 +1,7 @@
 export const fetchConfig = async () => {
+  console.log('Fetching config', process.env.NEXT_PUBLIC_URL);
   try {
-    const data = await fetch('http://localhost:3000/config.json').then(res => {
+    const data = await fetch(`${process.env.NEXT_PUBLIC_URL}/config.json`).then(res => {
       if (!res.ok) {
         throw new Error(`HTTP error! Status: ${res.status}`);
       }
